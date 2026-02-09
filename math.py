8592520b63520101e7f5f77a5153dc4caf30cf72
charliekieran <charliekieran53@gmail.com>
2026-02-09 14:54:34 +0300

    arithmetic

---
+++ math.py
@@ -0,0 +1,16 @@
+print("my" + " name " + "is " + "charlie")
+
+yt=5+3
+print(yt)
+multiply=5*3
+print(multiply)
+division=5/3
+print(division)
+sub=5-3
+print(sub)
+power=5**3
+print(power)
+modulus=5%3
+print(modulus)
+floorDivision=5//3
+print(floorDivision)
