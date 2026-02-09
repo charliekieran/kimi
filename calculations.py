1bb199ee2349c624639fa06152f6779a8e03d5ac
charliekieran <charliekieran53@gmail.com>
2026-02-09 15:34:03 +0300

    int

---
+++ calculations.py
@@ -0,0 +1,9 @@
+L=int(input("enter the length "))
+W=int(input("enter the width "))
+Area=L*W
+print(Area)
+Length=int(input("enter the Length"))
+Width=int(input("enter the width"))
+Height=int(input("enter the Height"))
+A=Length*Width*Height
+print(A)
