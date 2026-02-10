11162ed52fb14fb6e7b6af2bffccbb981d36aac8
charliekieran <charliekieran53@gmail.com>
2026-02-10 14:50:31 +0300

    assignment1

---
+++ caroline.py
@@ -0,0 +1,8 @@
+pens=21.25
+notebooks=23.50
+user_notebooks=float(input("enter the number of notebooks"))
+user_pens=float(input("enter the number of pens"))
+total_notebooks=user_notebooks*notebooks
+total_pens=user_pens*pens
+total_amount=total_notebooks+total_pens
+print(total_amount)
