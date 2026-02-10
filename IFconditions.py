45cff6bda7d36b09ee6a176dc3ab8683f4c08367
charliekieran <charliekieran53@gmail.com>
2026-02-10 15:59:22 +0300

    if

---
+++ IFconditions.py
@@ -0,0 +1,7 @@
+Name=input("what is your name ").lower()
+if Name=="caroline":
+    print("this is your name") 
+elif Name=="sam":
+    print("this is your trainer")
+else:
+    print("this is not your name")
